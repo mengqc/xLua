37573610c8351d43d37f4f2860645b7775c5adaf
qingchao.meng <qingchao.meng@centurygame.com>
2022-10-09 12:32:09 +0800

    new lib

---
+++ Assets/XLua/Src/ThirdPartLuaDLL.cs
@@ -0,0 +1,23 @@
+namespace XLua.LuaDLL
+{
+    public partial class Lua
+    {
+        [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int luaopen_pb(System.IntPtr L);
+
+        [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
+        public static int LoadProtobuf(System.IntPtr L)
+        {
+            return luaopen_pb(L);
+        }
+
+        [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int luaopen_rapidjson(System.IntPtr L);
+
+        [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
+        public static int LoadRapidJson(System.IntPtr L)
+        {
+            return luaopen_rapidjson(L);
+        }
+    }
+}
